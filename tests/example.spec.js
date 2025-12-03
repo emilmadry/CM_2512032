7628b1a2891486b30c965e47e15c1aa1a0a17d68
emilmadry <36087772+emilmadry@users.noreply.github.com>
2025-12-03 10:42:03 +0100

    test: added new test (#1)

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,11 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
+test('mad qa has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('Testowy Sklep – Strona główna');
+});
